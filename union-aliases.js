913a62cbab7a0cfb903d22269e5cd2ccf9db7805
ErnieBuyco <buycoernie@gmail.com>
2020-02-04 21:25:54 +0800

    types-aliases

---
+++ union-aliases.js
@@ -0,0 +1,23 @@
+// Union types:
+function combine(input1, input2, resultConversion) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+    // if (resultConversion === 'as-number'){
+    //         // return parseFloat(result);
+    //         return +result;
+    // } else {
+    //     return result;
+    // }
+}
+var combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+var combinedStringAges = combine('30', '36', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
